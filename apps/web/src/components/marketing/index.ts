dba8362dbe29f480404a63ba6f7a4254c9ddf244
Kelechi Odom <kelechiodom10@gmail.com>
2025-03-24 14:42:22 +0000

    feat: export all marketing sections

--- apps/web/src/components/marketing/index.ts
+++ apps/web/src/components/marketing/index.ts
@@ -1,3 +1,7 @@
 import Hero from "./hero.astro";
+import Features from "./features.astro";
+import HowItWorks from "./how-it-works.astro";
+import FAQs from "./faqs.astro";
+import NotFound from "./not-found.astro";
 
-export { Hero };
+export { Hero, Features, HowItWorks, FAQs, NotFound };
