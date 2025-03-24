223e2e6fdbf6127fe2343e92a842b488027873c7
Kelechi Odom <kelechiodom10@gmail.com>
2025-03-24 14:23:02 +0000

    feat: enhance auth module with rate limiting and view handler

--- apps/api/src/auth/index.ts
+++ apps/api/src/auth/index.ts
@@ -1,12 +1,20 @@
+import Elysia, { type Context } from "elysia";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { magicLink } from "better-auth/plugins";
 import { db } from "../db";
+import { users, sessions, verifications, accounts } from "../db/schemas";
 
-export const auth = betterAuth({
+const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
     usePlural: true,
+    schema: {
+      user: users,
+      session: sessions,
+      verification: verifications,
+      account: accounts,
+    },
   }),
   plugins: [
     magicLink({
@@ -21,4 +29,22 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     },
   },
+  rateLimit: {
+    enabled: true,
+  },
 });
+
+const betterAuthView = (context: Context) => {
+  const BETTER_AUTH_ACCEPT_METHODS = ["POST", "GET"];
+  // validate request method
+  if (BETTER_AUTH_ACCEPT_METHODS.includes(context.request.method)) {
+    return auth.handler(context.request);
+    // biome-ignore lint/style/noUselessElse: <explanation>
+  } else {
+    context.error(405);
+  }
+};
+
+const authService = new Elysia().all("/auth/*", betterAuthView);
+
+export { authService, auth, betterAuthView };
