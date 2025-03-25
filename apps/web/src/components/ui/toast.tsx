1cbbbf0856266ec373b835b1ec42e92a593f22bc
Kelechi Odom <kelechiodom10@gmail.com>
2025-03-25 20:50:09 +0000

    feat: update toast component

--- apps/web/src/components/ui/toast.tsx
+++ apps/web/src/components/ui/toast.tsx
@@ -1,28 +1,35 @@
-import { Toaster as ToasterPrimitive, type ToasterProps } from "sonner";
+import {
+  Toaster as ToasterPrimitive,
+  type ToasterProperties,
+} from "@pheralb/toast";
 import { useTheme } from "../theme-provider";
+import type { FunctionComponent } from "react";
+import { IconX } from "justd-icons";
 
-const Toast = ({ ...props }: ToasterProps) => {
+const Toast: FunctionComponent<ToasterProperties> = props => {
   const { theme = "system" } = useTheme();
+
   return (
     <ToasterPrimitive
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      richColors
+      theme={theme}
       toastOptions={{
+        font: "font-sans",
+        animationOnClose: "slide",
         classNames: {
-          toast: "toast border-0! inset-ring! inset-ring-fg/10!",
-          title: "title",
-          description: "description",
-          actionButton: "bg-primary! hover:bg-primary/90! text-primary-fg!",
-          cancelButton:
-            "bg-transparent! hover:bg-secondary! hover:text-secondary-fg!",
-          closeButton: "close-button",
+          toast: "toast border-0! inset-ring! inset-ring-fg/10! font-sans",
+          actions: {
+            actionBtn: "bg-primary! hover:bg-primary/90! text-primary-fg!",
+            closeBtn: "group",
+            container: "actions",
+          },
         },
+        defaultCloseContent: (
+          <IconX className="size-3 group-hover:text-muted-fg" />
+        ),
       }}
       {...props}
     />
   );
 };
 
-export type { ToasterProps };
 export { Toast };
