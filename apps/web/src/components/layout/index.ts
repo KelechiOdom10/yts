56c445a9f4cb485cf9f83a4a3a2a6004c135da1e
Kelechi Odom <kelechiodom10@gmail.com>
2025-03-25 17:04:58 +0000

    chore: make logo shared

--- apps/web/src/components/layout/index.ts
+++ apps/web/src/components/layout/index.ts
@@ -1,5 +1,6 @@
 import Head from "./head.astro";
 import Footer from "./footer.astro";
 import Header from "./header.astro";
+import Logo from "./logo.astro";
 
-export { Header, Footer, Head };
+export { Header, Footer, Head, Logo };
