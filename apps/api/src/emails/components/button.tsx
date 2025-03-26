fb78b1247bb60bf04a93c1938568bc6fd8b4bb8b
Kelechi Odom <kelechiodom10@gmail.com>
2025-03-26 19:19:05 +0000

    style: reduce margin y for email button

--- apps/api/src/emails/components/button.tsx
+++ apps/api/src/emails/components/button.tsx
@@ -2,7 +2,7 @@ import { Button, type ButtonProps, Section } from "@react-email/components";
 
 export const EmailButton = ({ children, href, ...props }: ButtonProps) => {
   return (
-    <Section className="my-8 first:mt-0 last:mb-0">
+    <Section className="my-6 first:mt-0 last:mb-0">
       <Button
         className="rounded-md bg-neutral-950 px-5 py-3 text-center font-medium text-sm text-white no-underline"
         href={href}
